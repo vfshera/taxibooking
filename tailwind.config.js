cfaafc404f0b4bf4a954d7e79d464b86f3bce666
Franklin Shera <fshera96@gmail.com>
2022-10-27 13:46:20 +0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2C83BC",
+      },
+    },
   },
   plugins: [],
 };
